7fcbf49b9148a314f7ff1ea6d2603015b02912ee
krs <cristianlaczko@yahoo.com>
2023-12-28 20:53:46 +0200

    Added tests for replacing subfolders

--- VCMI_populate_SW_assets_based_on_jsons/main.py
+++ VCMI_populate_SW_assets_based_on_jsons/main.py
@@ -8,7 +8,25 @@ def remove_comments(json_str):
     return re.sub(r'//[^\n]*', '', json_str)
 
 
-def get_animation_strings(data, file_name):
+def replace_content_with_sprites(path):
+    # Split the path into components
+    path_parts = path.split(os.path.sep)
+
+    # Find the index of 'content' case-insensitively
+    content_index = next((i for i, part in enumerate(path_parts) if part.lower() == 'content'), None)
+
+    if content_index is not None:
+        # Replace everything after 'content' with 'sprites'
+        path_parts[content_index + 1:] = ['sprites']
+        # Join the modified components back into a path
+        modified_path = os.path.join(*path_parts)
+        return modified_path
+    else:
+        # If 'content' is not found, return the original path
+        return path
+
+
+def get_animation_strings(data, json_file_path, folder_path):
     animation_strings = set()
 
     def explore(node):
@@ -17,7 +35,14 @@ def get_animation_strings(data, file_name):
                 if key == "animation" and isinstance(value, str):
                     # Check if the animation file has .def extension, if not, add it
                     animation_file = value + (".def" if not value.endswith(".def") else "")
-                    animation_strings.add(animation_file)
+                    relative_path_to_mods = os.path.relpath(json_file_path, folder_path)
+                    # Replace 'config' with 'sprites' in the path
+                    full_animation_path = replace_content_with_sprites(relative_path_to_mods)
+                    full_animation_path = os.path.join(
+                        relative_path_to_mods,
+                        animation_file
+                    )
+                    animation_strings.add(full_animation_path)
                 else:
                     explore(value)
         elif isinstance(node, list):
@@ -28,13 +53,13 @@ def get_animation_strings(data, file_name):
     return animation_strings
 
 
-def parse_json_file(file_path):
+def parse_json_file(file_path, folder_path):
     try:
         with open(file_path, 'r') as file:
             json_content = file.read()
             json_content_without_comments = remove_comments(json_content)
             json_data = json.loads(json_content_without_comments)
-            animation_strings = get_animation_strings(json_data, os.path.basename(file_path))
+            animation_strings = get_animation_strings(json_data, file_path, folder_path)
             return animation_strings
 
     except FileNotFoundError:
@@ -56,7 +81,7 @@ def parse_folder(folder_path):
         for file in files:
             if file.endswith('.json'):
                 file_path = os.path.join(root, file)
-                animation_strings = parse_json_file(file_path)
+                animation_strings = parse_json_file(file_path, folder_path)
                 all_animation_strings.update(animation_strings)
 
     return all_animation_strings
